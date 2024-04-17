a03e74c8b1264d95710201f733e09ec97f3d6f4a
alvarez15888 <alvarez15888@gmail.com>
2024-04-17 15:27:00 +0200

    20240417

---
+++ Aula01/src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'toolbar-component',
+  templateUrl: './toolbar.component.html'
+})
+export class ToolbarComponent {
+  welcome = 'Bienvenido';
+}
